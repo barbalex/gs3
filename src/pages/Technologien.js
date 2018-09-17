2e96bad99ea62cd8bfc47f10e081694b5c9ec04d
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-18 01:25:03 +0200

    style

--- src/pages/Technologien.js
+++ src/pages/Technologien.js
@@ -6,111 +6,128 @@ import {
   FaApple,
   FaWindows,
 } from 'react-icons/fa'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 
+const H4 = styled.h4`
+  margin-top: 5px;
+  font-weight: bold;
+`
+const A = styled.a`
+  color: #333;
+  border-bottom: 1px dashed #333;
+  &:hover {
+    border-bottom: 1px solid #428bca;
+    text-decoration: none;
+  }
+`
+const P = styled.p`
+  margin-bottom: -2px;
+`
+
 const TechnologienPage = () => (
   <Layout>
-    <div id="technologien" className="page technologien not_home">
+    <div id="technologien" className="page technologien">
       <p className="lead">Mit diesen Technologien kenne ich mich aus:</p>
       <div className="row marketing">
         <div className="col-lg-4">
           <FaLinux style={{ fontSize: 30 }} alt="Linux" />
           <FaApple style={{ fontSize: 30 }} alt="MaxOs" />
           <FaWindows style={{ fontSize: 30 }} alt="Windows" />
-          <h4>Betriebssysteme</h4>
+          <H4>Betriebssysteme</H4>
           <ul>
             <li>
-              <a
+              <A
                 href="http://de.wikipedia.org/wiki/Microsoft_Windows"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Windows
-              </a>
-              <p>Der Benutzerfreundliche</p>
+              </A>
+              <P>Der Benutzerfreundliche</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://de.wikipedia.org/wiki/Ubuntu"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Ubuntu Linux
-              </a>
-              <p>Der Server-Meister</p>
+              </A>
+              <P>Der Server-Meister</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://de.wikipedia.org/wiki/Mac_OS_X"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 macOS
-              </a>
-              <p>Entwickler fliegen drauf...</p>
+              </A>
+              <P>Entwickler fliegen drauf...</P>
             </li>
           </ul>
           <FaDatabase style={{ fontSize: 30 }} />
-          <h4 style={{ marginTop: 5 }}>Datenbanken</h4>
+          <H4 style={{ marginTop: 5 }}>Datenbanken</H4>
           <ul>
             <li>
-              <a
+              <A
                 href="http://www.postgresql.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 PostgreSQL
-              </a>
-              <p>Der Klassenprimus</p>
+              </A>
+              <P>Der Klassenprimus</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://couchdb.apache.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 CouchDB
-              </a>
-              <p>Der Synchronisations-Meister</p>
+              </A>
+              <P>Der Synchronisations-Meister</P>
             </li>
             <li>
-              <a
+              <A
                 href="https://pouchdb.com"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 PouchDB
-              </a>
-              <p>CouchDB im Browser</p>
+              </A>
+              <P>CouchDB im Browser</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://www.sqlite.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 SQLite
-              </a>
-              <p>{`Simpler geht's nimmer`}</p>
+              </A>
+              <P>{`Simpler geht's nimmer`}</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://www.mysql.de"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 MySQL
-              </a>
+              </A>
             </li>
             <li>
-              <a
+              <A
                 href="http://de.wikipedia.org/wiki/Microsoft_Access"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Microsoft Access
-              </a>
+              </A>
             </li>
           </ul>
         </div>
@@ -126,243 +143,243 @@ const TechnologienPage = () => (
           >
             {'if ( ... ) then \u007B ... \u007D'}
           </div>
-          <h4 style={{ marginTop: 3 }}>Programmiersprachen</h4>
+          <H4 style={{ marginTop: 3 }}>Programmiersprachen</H4>
           <ul>
             <li>
-              <a
+              <A
                 href="http://en.wikipedia.org/wiki/JavaScript"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 JavaScript
-              </a>
-              <p>
+              </A>
+              <P>
                 Die verbreitetste Programmiersprache. Vermutlich auch die
                 vielfältigste und sich am schnellsten weiter entwickelnde
-              </p>
+              </P>
             </li>
             <li>
-              <a
+              <A
                 href="http://de.wikipedia.org/wiki/JavaScript_Object_Notation"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 JSON
-              </a>
-              <p>Hierarchische Datenstrukturen</p>
+              </A>
+              <P>Hierarchische Datenstrukturen</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://de.wikipedia.org/wiki/Cascading_Style_Sheets"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 CSS
-              </a>
-              <p>Layouten</p>
+              </A>
+              <P>Layouten</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://de.wikipedia.org/wiki/Hypertext_Markup_Language"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 HTML
-              </a>
+              </A>
             </li>
             <li>
-              <a
+              <A
                 href="http://de.wikipedia.org/wiki/SQL"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 SQL
-              </a>
-              <p>
+              </A>
+              <P>
                 Nach wie vor der Standard, um Daten abzufragen und zu
                 manipulieren
-              </p>
+              </P>
             </li>
           </ul>
         </div>
         <div className="col-lg-4">
           <FaWrench style={{ fontSize: 30 }} />
-          <h4 style={{ marginTop: 0 }}>
+          <H4 style={{ marginTop: 0 }}>
             Werkzeuge <small>(Auswahl)</small>
-          </h4>
+          </H4>
           <ul>
             <li>
-              <a
+              <A
                 href="http://http://nodejs.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 node.js
-              </a>
-              <p>JavaScript auf PC und Server</p>
+              </A>
+              <P>JavaScript auf PC und Server</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://electron.atom.io"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 electron
-              </a>
-              <p>JavaScript-Anwendungen lokal installieren</p>
+              </A>
+              <P>JavaScript-Anwendungen lokal installieren</P>
             </li>
             <li>
-              <a
+              <A
                 href="https://flow.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 flow
-              </a>
-              <p>Findet Fehler, bevor der Code ausgeführt wird</p>
+              </A>
+              <P>Findet Fehler, bevor der Code ausgeführt wird</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://hapijs.com"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 hapi
-              </a>
-              <p>Meister Server</p>
+              </A>
+              <P>Meister Server</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://webpack.github.io"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 webpack
-              </a>
-              <p>Entwicklungsumgebung konfigurieren</p>
+              </A>
+              <P>Entwicklungsumgebung konfigurieren</P>
             </li>
             <li>
-              <a
+              <A
                 href="https://facebook.github.io/react"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 React
-              </a>
-              <p>
+              </A>
+              <P>
                 Die Benutzeroberfläche ist eine Funktion der Anwendungsdaten
-              </p>
+              </P>
             </li>
             <li>
-              <a
+              <A
                 href="//graphql.org/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 GraphQL
-              </a>
+              </A>
               &nbsp;und&nbsp;
-              <a
+              <A
                 href="https://www.apollographql.com/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Apollo
-              </a>
-              <p>Die Anwendungsdaten sind eine Funktion der Abfrage</p>
+              </A>
+              <P>Die Anwendungsdaten sind eine Funktion der Abfrage</P>
             </li>
             <li>
-              <a
+              <A
                 href="https://mobx.js.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 MobX
-              </a>
+              </A>
               ,{' '}
-              <a
+              <A
                 href="https://github.com/mobxjs/mobx-state-tree"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 MobX-State-Tree
-              </a>
-              <p>Meister im Manipulieren von Anwendungsdaten</p>
+              </A>
+              <P>Meister im Manipulieren von Anwendungsdaten</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://getbootstrap.com/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Bootstrap
-              </a>
+              </A>
               &nbsp;und&nbsp;
-              <a
+              <A
                 href="http://www.material-ui.com"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Material UI
-              </a>
-              <p>Layouten nach Google und Microsoft</p>
+              </A>
+              <P>Layouten nach Google und Microsoft</P>
             </li>
             <li>
-              <a
+              <A
                 href="https://github.com/styled-components/styled-components"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 styled-components
-              </a>
-              <p>CSS für Komponenten</p>
+              </A>
+              <P>CSS für Komponenten</P>
             </li>
             <li>
-              <a
+              <A
                 href="http://docs.couchdb.org/en/2.0.0/couchapp"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 CouchApp
-              </a>
-              <p>
+              </A>
+              <P>
                 CouchDB-Datenbank, Server und Anwendung in Einem! Läuft und
                 läuft und läuft...
-              </p>
+              </P>
             </li>
             <li>
-              <a
+              <A
                 href="http://openlayers.org/"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 OpenLayers
-              </a>
+              </A>
               ,&nbsp;
-              <a
+              <A
                 href="http://leafletjs.com"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Leaflet
-              </a>
+              </A>
               ,&nbsp;
-              <a
+              <A
                 href="http://www.geo.admin.ch/internet/geoportal/de/home/services/geoservices/display_services/api_services.html"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 GeoAdmin API
-              </a>
+              </A>
               &nbsp;und&nbsp;
-              <a
+              <A
                 href="https://developers.google.com/maps/?hl=de"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Google Maps API
-              </a>
-              <p>Karten einbinden</p>
+              </A>
+              <P>Karten einbinden</P>
             </li>
           </ul>
         </div>
