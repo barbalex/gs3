7b9ef089cf2604a38ab8e2600fd1879c469fd4a9
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-22 21:26:54 +0200

    update bb

--- src/modules/bbCarouselItems.js
+++ src/modules/bbCarouselItems.js
@@ -1,6 +1,10 @@
-import bb1Img from '../images/bb1.png'
-import bb2Img from '../images/bb2.png'
-import bb3Img from '../images/bb3.png'
+import bb1Img from '../images/mm_01.jpg'
+import bb2Img from '../images/mm_02.jpg'
+import bb3Img from '../images/mm_03.jpg'
+import bb4Img from '../images/mm_04.jpg'
+import bb5Img from '../images/mm_05.jpg'
+import bb6Img from '../images/mm_06.jpg'
+import bb7Img from '../images/mm_07.jpg'
 
 export default [
   {
@@ -20,5 +24,29 @@ export default [
     altText: 'Kommentare',
     caption: '',
     header: 'Kommentare',
+  },
+  {
+    src: bb4Img,
+    altText: 'Kommentar',
+    caption: '',
+    header: 'Kommentar',
+  },
+  {
+    src: bb5Img,
+    altText: 'Neues Ereignis erstellen',
+    caption: '',
+    header: 'Neues Ereignis erstellen',
+  },
+  {
+    src: bb6Img,
+    altText: 'Ereignis bearbeiten',
+    caption: '',
+    header: 'Ereignis bearbeiten',
+  },
+  {
+    src: bb7Img,
+    altText: 'Kommentar bearbeiten',
+    caption: '',
+    header: 'Kommentar bearbeiten',
   },
 ]
