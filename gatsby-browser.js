ae7a497ca5dc32c1a0de1526e6c94ef04297b15d
Alexander Gabriel <alex.barbalex@gmail.com>
2019-03-21 16:07:28 +0100

    update service worker more quickly

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,6 @@
  */
 
 // You can delete this file if you're not using it
+
+// https://github.com/gatsbyjs/gatsby/issues/9087#issuecomment-459105021
+export const onServiceWorkerUpdateFound = () => window.location.reload(true)
